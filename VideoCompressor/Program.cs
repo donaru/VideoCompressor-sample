e02629b9666fb87ed437de2cc261fc0225c3540f
DESKTOP-K3B2M7Q\Donald <chan_donald@Hotmail.com>
2017-05-04 10:55:12 +1000

    no message

---
+++ VideoCompressor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Nuget - MencoderSharp
+
+namespace VideoCompressor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+
+            ffMpeg.ConvertMedia("example.mpg", "video.mp4", "mp4");
+
+        }
+    }
+}
